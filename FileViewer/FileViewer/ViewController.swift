2eeef1364facf21bdc0bcd3b271db4dd0907ef27
Mark Sharvin <marksharvin@gmail.com>
2019-07-23 22:47:02 +0100

    Adding functionality from the tutorial

--- FileViewer/FileViewer/ViewController.swift
+++ FileViewer/FileViewer/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: NSViewController {
 
   @IBOutlet weak var statusLabel: NSTextField!
 
+  @IBOutlet weak var fileListTable: NSTableView!
   let sizeFormatter = ByteCountFormatter()
   var directory: Directory?
   var directoryItems: [Metadata]?
@@ -34,16 +35,129 @@ class ViewController: NSViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    statusLabel.stringValue = ""
+    statusLabel.stringValue = "No Items"
+    setupFileList()
+  }
+  
+  func setupFileList() {
+    fileListTable.dataSource = self
+    fileListTable.delegate = self
+    fileListTable.target = self
+    fileListTable.doubleAction = #selector(tableViewDoubleClick(_:))
+    
+    fileListTable.tableColumns.first?.sortDescriptorPrototype = NSSortDescriptor(key: Directory.FileOrder.Name.rawValue, ascending: true)
+    fileListTable.tableColumns[1].sortDescriptorPrototype = NSSortDescriptor(key: Directory.FileOrder.Date.rawValue, ascending: true)
+    fileListTable.tableColumns.last?.sortDescriptorPrototype = NSSortDescriptor(key: Directory.FileOrder.Size.rawValue, ascending: true)
+    
   }
 
   override var representedObject: Any? {
-    didSet {
-      if let url = representedObject as? URL {
-        print("Represented object: \(url)")
-
+      didSet {
+          if let url = representedObject as? URL {
+            directory = Directory(folderURL: url)
+            reloadFileList()
+          }
       }
+  }
+  
+  func reloadFileList() {
+    directoryItems = directory?.contentsOrderedBy(sortOrder, ascending: sortAscending)
+    fileListTable.reloadData()
+  }
+  
+  func updateStatus() {
+    var statusMessage = ""
+    let itemsSelected = fileListTable.selectedRowIndexes.count
+    if directoryItems == nil {
+      statusMessage = "No Items"
+    } else if itemsSelected == 0 {
+      statusMessage = "\(directoryItems!.count) items"
+    } else {
+      statusMessage = "\(itemsSelected) of \(directoryItems!.count) selected"
+    }
+    statusLabel.stringValue = statusMessage
+  }
+  
+  @objc func tableViewDoubleClick(_ sender:AnyObject) {
+    guard fileListTable.selectedRow >= 0, let item = directoryItems?[fileListTable.selectedRow] else {
+      return
+    }
+    
+    if item.isFolder {
+      representedObject = item.url as Any
+    } else {
+      NSWorkspace.shared.open(item.url as URL)
     }
   }
+  
+}
+
+extension ViewController: NSTableViewDelegate {
+  
+  fileprivate enum CellIdentifiers {
+    static let NameCell = "NameCellID"
+    static let DateCell = "DateCellID"
+    static let SizeCell = "SizeCellID"
+  }
+  
+  func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+    guard let item = directoryItems?[row] else {
+      return nil
+    }
+    
+    var image: NSImage?
+    var cellIdentifier = ""
+    
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = .long
+    dateFormatter.timeStyle = .long
+    
+    var text = ""
+    if tableColumn == tableView.tableColumns[0] {
+      image = item.icon
+      text = item.name
+      cellIdentifier = CellIdentifiers.NameCell
+    } else if tableColumn == tableView.tableColumns[1] {
+      text = dateFormatter.string(from: item.date)
+      cellIdentifier = CellIdentifiers.DateCell
+    } else if tableColumn == tableView.tableColumns[2] {
+      text = item.isFolder ? "--" : sizeFormatter.string(fromByteCount: item.size)
+      cellIdentifier = CellIdentifiers.SizeCell
+    }
+    
+    let identifier = NSUserInterfaceItemIdentifier(cellIdentifier)
+    if let cell = tableView.makeView(withIdentifier: identifier, owner: nil) as? NSTableCellView {
+      cell.textField?.stringValue = text
+      cell.imageView?.image = image
+      return cell
+    }
+    
+    return nil
+  }
+  
+  func tableViewSelectionDidChange(_ notification: Notification) {
+    updateStatus()
+  }
+  
+}
+
+extension ViewController: NSTableViewDataSource {
+  
+  func numberOfRows(in tableView: NSTableView) -> Int {
+    return directoryItems?.count ?? 0
+  }
+  
+  func tableView(_ tableView: NSTableView, sortDescriptorsDidChange oldDescriptors: [NSSortDescriptor]) {
+    guard let sortDescriptor = tableView.sortDescriptors.first else {
+      return
+    }
+    
+    if let order = Directory.FileOrder(rawValue: sortDescriptor.key!) {
+      sortOrder = order
+      sortAscending = sortDescriptor.ascending
+      reloadFileList()
+    }
+  }
+  
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # FileViewer
-NSTableView tutorial
+###### Mac app NSTableView tutorial that makes a basic file viewer
+
+The reason behind me creating this is I wanted to learn more about building mac apps, specifically interaction with NSTableViews, so I can improve on my iOS development knowledge.
+This project follows the raywenderlich.com tutorial [macOS NSTableTutorial](https://www.raywenderlich.com/830-macos-nstableview-tutorial) where we create a very basic app that displays the contents of a folder.
+Topics covered:
+- Populating a table view
+- Changing it's visual stye
+- React to user interaction like selections, double clicks and sorting
